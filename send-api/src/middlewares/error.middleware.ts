ca8115d019a8bee833dc5848a2a460101c6df32c
Rob De Feo <robdefeo@gmail.com>
2022-11-23 13:58:41 +0100

    chore: remove-linting
    Attempting to simplify example code so removing linting.
    
    GitOrigin-RevId: 5e06673c549db9c00fcee2058bdde0a8d0aaa18a

--- send-api/src/middlewares/error.middleware.ts
+++ send-api/src/middlewares/error.middleware.ts
@@ -2,9 +2,9 @@ import { NextFunction, Request, Response } from 'express';
 import { HttpException } from '@exceptions/HttpException';
 
 const errorMiddleware = (error: HttpException, req: Request, res: Response, next: NextFunction) => {
-  try {
-    const status: number = error.status || 500;
-    const message: string = error.message || 'Something went wrong';
+	try {
+		const status: number = error.status || 500;
+		const message: string = error.message || 'Something went wrong';
 
     console.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}`);
     res.status(status).json({ message });
